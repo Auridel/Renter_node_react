1f89b696e5a0c793545571db513f4265f6bc5c09
Auridel <31318127+Auridel@users.noreply.github.com>
2020-10-25 19:54:44 +0300

    deploy test

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const loginRouter = require("./routes/login");
 const accountRouter = require("./routes/account");
-const {MONGODB_URI} = require("./keys");
+// const {MONGODB_URI} = require("./keys");
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 app.use(cors());
@@ -14,7 +15,14 @@ app.use(express.json({extended: true}))//json
 app.use("/api/auth", loginRouter);
 app.use("/api/account", accountRouter);
 
-app.use(express.static(path.join(__dirname, "public")));
+if(process.env.NODE_ENV === "production"){
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
 
 const PORT = process.env.PORT || 4000;
 
